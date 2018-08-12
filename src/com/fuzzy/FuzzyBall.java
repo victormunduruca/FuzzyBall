dfd56eb24af154f43562ecc649744ad9ac6ccbd9
teixeirista <teixeirista@gmail.com>
2018-08-12 15:06:05 -0300

    Simulation changes (with some problems)

--- src/com/fuzzy/FuzzyBall.java
+++ src/com/fuzzy/FuzzyBall.java
@@ -8,43 +8,69 @@ public class FuzzyBall {
 		String filename = "tipper.fcl";
 		FIS fis = FIS.load(filename, true);
 		double dx, dy;
+		double speedAd, angleAd;
+		boolean hited = false;
 
 		if (fis == null) {
 			System.err.println("Can't load file: '" + filename + "'");
 			System.exit(1);
 		}
 
-		Simulation simulation = new Simulation(3, 2, 1);
-		double speed = 10, angle = Math.toRadians(45);
-		int i = 0;
-		while(i < 10) {
-			System.out.println("Velocidade: " +speed + "Angle: " +angle);
-
+		Simulation simulation = new Simulation(10, 5, 1);
+		double speed = 5, angle = Math.toDegrees(45);
+		int i = 1;
+		while(!hited) {
+			System.out.println("Tentativa " + i);
+			System.out.println("Entradas");
+			System.out.println("Speed: " +speed + " || Angle: " +angle);
+			System.out.println();
 			
+			hited = simulation.simulate(angle, speed);
+			
+			if(hited)
+				System.out.println("Acertô, miserave");
+			else {
 
-			simulation.simulate(angle, speed);
-
-			dx = simulation.getDX();
-			dy = simulation.getDY();
-			System.out.println(dx);
-			System.out.println(dy);
-
-			//		
-			//		// Get default function block
-			FunctionBlock fb = fis.getFunctionBlock(null);
-			//
-			//		// Set inputs
-			fb.setVariable("dx", dx);
-			fb.setVariable("dy", dy);
-			//
-			//		// Evaluate
-			fb.evaluate();
-			//
-			//		// Show output variable's chart
-			speed = fb.getVariable("speed").defuzzify();
-			angle = fb.getVariable("angle").defuzzify();
-
-			System.out.println("Velocidade: " +speed + "Angle: " +angle);
+				dx = simulation.getDX();
+				dy = simulation.getDY();
+				System.out.println("Diferenças:");
+				System.out.println("DX: " + dx + " || DY: " + dy);
+				System.out.println();
+	
+				//		
+				//		// Get default function block
+				FunctionBlock fb = fis.getFunctionBlock(null);
+				//
+				//		// Set inputs
+				fb.setVariable("dx", dx);
+				fb.setVariable("dy", dy);
+				//
+				//		// Evaluate
+				fb.evaluate();
+				
+				speedAd = fb.getVariable("speed").defuzzify();
+				angleAd = fb.getVariable("angle").defuzzify();
+				
+				System.out.println("Ajustes");
+				System.out.println("Velocidade: " +speedAd + " || Ajuste ângulo: " + angleAd);
+				System.out.println();
+				//
+				//		// Show output variable's chart
+				speed += speedAd;
+				angle += angleAd;
+				
+				/*if(angle > 90)
+					angle = 90;
+				else if(angle < 0)
+					angle = 0;
+				
+				if(speed > 20)
+					speed = 20;
+				else if(speed < 1)
+					speed = 1;*/
+			}
+			
+			i++;
 		}
 
 		//		// Print ruleSet
--- src/com/fuzzy/Simulation.java
+++ src/com/fuzzy/Simulation.java
@@ -2,7 +2,8 @@ package com.fuzzy;
 
 public class Simulation {
 	private double xA, yA, yC, time, xR, yR;
-	private double vX, vY, x, y;
+	private double vX, vY, x = 0, y;
+	private double radius = 1.5;
 	
 	private final double g = 9.8;
 
@@ -13,28 +14,34 @@ public class Simulation {
 		this.yC = yC;
 	}
 	
-	public void simulate(double angle, double velocity) {
+	public boolean simulate(double angle, double velocity) {
 		time = 0;
 		xR = 0;
 		yR = 0;
 		
 		y = yC;
-		double hmax = (velocity*velocity*Math.sin(angle)*Math.sin(angle))/(2*g);
-		System.out.println("Vy: " +hmax);
+		//double hmax = (velocity*velocity*Math.sin(angle)*Math.sin(angle))/(2*g);
+		//System.out.println("Vy: " +hmax);
 		
-		while(y > 0) { //while don't heat the floor
+		while(y > 0) { //while don't hit the floor
+			
 			vX = velocity*Math.cos(angle);//calculate vx and vy
 			vY = velocity*Math.sin(angle);
 			x = vX*time; //calculate x coordinate 
 			y = yC + vY*time - (g*(time*time))/2; //calculate y coordinate
 			
-			//System.out.println(x+" "+y);
-			if(x == xA) { //check if yR for x coordinate equals xA
+			System.out.println(y + " | " + x);
+			
+			if(x < (xA + radius) && x > (xA - radius)) //check if yR for x coordinate equals xA
 				yR = y;
-			}
+			
+			if(y > (yA - radius) && y < (yA + radius) && x < (xA + radius) && x > (xA - radius))
+				return true;
+			
 			time = time + 0.1; //increments time
-		} 
-		xR = x;	//TODO 		
+		}
+		xR = x;	//TODO
+		return false;
   	}
 	
 	public double getDX() {
